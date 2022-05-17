1af37b2e2b89bd9c98b7ce7d5f92d98a86dd01cc
Ansh Doshi <anshdoshi2305@gmail.com>
2022-05-18 00:29:37 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -3,11 +3,14 @@
 const withPWA = require("next-pwa");
 
 module.exports = withPWA({
+  reactStrictMode: true,
+
   pwa: {
     dest: "public",
+    register: true,
+    skipWaiting: true,
     disable: process.env.NODE_ENV === "development",
   },
-  reactStrictMode: true,
   images: {
     domains: ["localhost", "res.cloudinary.com"],
   },
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,7 @@
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <div>
-      <link rel="manifest" href="/manifest.json" />
-
-      <Component {...pageProps} />
-    </div>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
